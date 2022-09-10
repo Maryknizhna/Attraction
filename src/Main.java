de55d66de77846cf3271b1eaf87e97cf45c86cc1
Maria Schevtsova <hatulmadan5002@gmail.com>
2022-09-10 11:20:01 +0300

    Main

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Person> queue = (Queue<Person>) generateClients();
+
+        while (!queue.isEmpty()) {
+            Person person = queue.poll();
+            System.out.println(person.name + " " + person.surname + " прокатился на аттракционе.");
+            person.tickets--;
+            if (person.tickets > 0) {
+                queue.add(person);
+            }
+        }
+    }
+    public static List<Person> generateClients() {
+        List<Person> persons = new LinkedList<>();
+        persons.add(new Person("Лари", "Шварц", 10));
+        persons.add(new Person("Мери", "Иствуд", 6));
+        persons.add(new Person("Валя", "Иванова", 20));
+        return persons;
+    }
+
+
+}
